a52ef681915c437f5c17a488de3c8a596a360fbb
Ture Gjørup <ture@ignatz.dk>
2022-07-07 11:27:52 +0200

    DDBTEAM-826: Added rector dev dependency

--- rector.php
+++ rector.php
@@ -1,13 +1,26 @@
 <?php
 
-use Rector\Symfony\Set\SymfonySetList;
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Symfony\Set\SymfonySetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->symfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
+    $rectorConfig->paths([
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ]);
 
+    $rectorConfig->symfonyContainerXml(__DIR__.'/var/cache/dev/App_KernelDevDebugContainer.xml');
+
+    // register a single rule
+    $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
+
+    // define sets of rules
     $rectorConfig->sets([
-        \Rector\Set\ValueObject\LevelSetList::UP_TO_PHP_81,
+        LevelSetList::UP_TO_PHP_81,
         SymfonySetList::SYMFONY_60,
         SymfonySetList::SYMFONY_CODE_QUALITY,
         SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
