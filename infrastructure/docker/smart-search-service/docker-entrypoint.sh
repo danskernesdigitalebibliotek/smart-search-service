724c28dd159dd9b4b99501d2e34e74074e01aa57
Jesper Kristensen <cableman@linuxdev.dk>
2022-07-12 13:29:06 +0200

    DDBTEAM-1024: Fixed missing helm configuration mapping

--- infrastructure/docker/smart-search-service/docker-entrypoint.sh
+++ infrastructure/docker/smart-search-service/docker-entrypoint.sh
@@ -11,10 +11,6 @@ set -eux
 ## Warm-up symfony cache (with the current configuration).
 /var/www/html/bin/console --env=prod cache:warmup --quiet
 
-## Create database (SQLite)
-/var/www/html/bin/console doctrine:database:create --no-interaction --quiet
-/var/www/html/bin/console doctrine:migrations:migrate --no-interaction --quiet
-
 ## Start the PHP process and run command if given. This trick is need to cron imports as k8s cron jobs.
 if [ $# -eq 0 ]
  then
