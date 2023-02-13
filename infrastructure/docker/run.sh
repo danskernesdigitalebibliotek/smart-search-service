3a56208b5b555290ad19b54bd134d5e2a62e581e
Jesper Kristensen <cableman@linuxdev.dk>
2023-02-13 14:55:41 +0100

    COVER-33: Updated docker images

--- infrastructure/docker/run.sh
+++ infrastructure/docker/run.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+set -e
+
 APP_VERSION=develop
 VERSION=alpha
+
+docker pull nginxinc/nginx-unprivileged:alpine
 
 docker build --no-cache --build-arg APP_VERSION=${APP_VERSION} --tag=danskernesdigitalebibliotek/smart-search-service:${VERSION} --file="smart-search-service/Dockerfile" smart-search-service
 docker build --no-cache --build-arg VERSION=${VERSION} --tag=danskernesdigitalebibliotek/smart-search-service-nginx:${VERSION} --file="nginx/Dockerfile" nginx
